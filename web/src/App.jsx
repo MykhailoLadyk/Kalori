fab15783e6cd5f228ef717c66c1265283eb5ca9c
MykhailoLadyk <lp346426201@gmail.com>
2026-03-11 12:56:06 +0100

    initial commit

---
+++ web/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1>Kalori</h1>
+      <div className="card"></div>
+    </>
+  );
+}
+
+export default App;

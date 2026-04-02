c07e5965c677d4fe7e5d3c722466bc1048414c4d
MykhailoLadyk <lp346426201@gmail.com>
2026-04-02 12:18:37 +0200

    fix: remove  old import

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,6 +1,4 @@
-// App will have all context providers. will also have all the pages and currentPage state. navbar always visible at the bottom
 import { useState } from "react";
-import "./App.css";
 import Home from "./pages/Home";
 import Nav from "./components/shared/Nav";
 function App() {
